4052af66d479374d8d78dff21f8db7615f45929b
Jhordy11 <fjhordy1@gmail.com>
2022-12-31 03:22:51 -0500

    Initial commit

---
+++ src/main/java/com/hotelalura/factory/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package com.hotelalura.factory;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.mchange.v2.c3p0.DataSources;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private DataSource dataSource;
+    public ConnectionFactory(){
+        var pooledDataSource = new ComboPooledDataSource();
+        pooledDataSource.setJdbcUrl("tuUrlALaBaseDeDatos");
+        pooledDataSource.setUser("tuUsuario");
+        pooledDataSource.setPassword("tuContraseña");
+        pooledDataSource.setMaxPoolSize(10);
+        this.dataSource = pooledDataSource;
+    }
+    public Connection recuperaConexion(){
+        try {
+            return this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
